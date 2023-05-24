89dc0320ad3a07a203d7a587cadb0c73e4422a37
hardy6413 <irreftw@gmail.com>
2023-05-24 13:33:49 +0200

    init postgres and mongo

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+import psycopg2
+import pymongo
+from pymongo import MongoClient
 
+postgresConn = psycopg2.connect(host="localhost", dbname="pz", user="pz", password="pz")
+postgresCursor = postgresConn.cursor()
+
+mongoClient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = mongoClient["logs"]
 
 if __name__ == '__main__':
     print("im working")
