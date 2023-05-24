591457d491f0a1ea7ac66f25eae16e0218d3125c
hardy6413 <irreftw@gmail.com>
2023-05-24 13:37:04 +0200

    instruction

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import pymongo
 from pymongo import MongoClient
 
 postgresConn = psycopg2.connect(host="localhost", dbname="pz", user="pz", password="pz")
-postgresCursor = postgresConn.cursor()
 
 mongoClient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = mongoClient["logs"]
+
+#tak sie dodaje do postgresa cos
+#postgresCursor = postgresConn.cursor()
+#postgresConn.commit()
+#postgresCursor.close()
+#postgresConn.close()
 
 if __name__ == '__main__':
     print("im working")
