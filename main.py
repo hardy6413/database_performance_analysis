dc1d830e2c2a9e4f2b5cfcabb91816bfab2f70b6
hardy6413 <irreftw@gmail.com>
2023-05-27 11:11:06 +0200

    added query for mongo and postgres

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import psycopg2
 import pymongo
-from sqlalchemy import create_engine, delete, MetaData
+from sqlalchemy import create_engine, delete, MetaData, select
 
 postgresConn = psycopg2.connect(host="localhost", dbname="pz", user="pz", password="pz")
 postgresConn.autocommit = True
@@ -16,14 +16,6 @@ mongoCollection = mongodb["data"]
 meta = MetaData(bind=alchemyDb)
 MetaData.reflect(meta)
 
-# tak sie dodaje do postgresa cos
-#postgresCursor = postgresConn.cursor()
-# postgresCursor.execute("""CREATE TABLE IF NOT EXISTS person (
-# id INT PRIMARY KEY )""")
-# postgresConn.commit()
-# postgresCursor.close()
-# postgresConn.close()
-
 # mierzenie czasau
 # start = time.time()
 # print("hello")
@@ -36,10 +28,9 @@ def addToPostgres(data):
 
 def deleteAllFromPostgres():
     postgresCursor = postgresConn.cursor()
-    postgresCursor.execute("""DELETE from "Data" where TRUE""")
+    postgresCursor.execute("""DELETE from "data" where TRUE""")
     postgresConn.commit()
     postgresCursor.close()
-    postgresConn.close()
 
 
 def deleteAllFromMongo():
@@ -48,6 +39,16 @@ def deleteAllFromMongo():
 
 def addToMongo(data):
     mongoCollection.insert_many(data.to_dict("records"))
+
+
+def executePostgresQuery(stmt):
+    res = pd.read_sql_query(stmt, postgresConn)
+    return res
+
+def executeMongoQuery(stmt):
+    x = mongoCollection.find(stmt)
+    df =  pd.DataFrame(list(x))
+    return df
 
 
 if __name__ == '__main__':
@@ -59,5 +60,8 @@ if __name__ == '__main__':
     deleteAllFromMongo()
     addToPostgres(chunk)
     addToMongo(chunk)
+    executePostgresQuery("SELECT * from data")
+    executeMongoQuery({"attacking_volleys": 88})
     print(chunk.head(5))
     print("im working")
+    postgresConn.close()
